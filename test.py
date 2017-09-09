31cd2f7efe8d9dd280f9d5e272977498449840be
briant1234 <btao.person@gmail.com>
2017-09-09 11:16:25 -0400

    pretty print json

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 print("Hello world")
-
+import json
 import requests
 response = requests.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=193675&t.k=dKoyQlorYWm&action=employers&q=software&userip=192.168.43.42&useragent=Chrome/43.0.2357.81', headers={
                                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"})
 print(response.status_code)
-
+data = response.json()
+f=open('response.json','w')
+print(data)
+formatted=str(json.dumps(data,sort_keys=True,indent=4, separators=(',', ': ')))
+f.write(formatted)
