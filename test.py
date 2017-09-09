b6844f0052bbe89f77742a6b09ff2b7364b516fe
jennywzhu <jenny.zhu84@gmail.com>
2017-09-09 10:07:28 -0400

    yes

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 print("Hello world")
+
+from pip.vendor import requests
+response = requests.get('http://api.glassdoor.com/api/api.htm?v=1.1&format=json&t.p=193675&t.k=dKoyQlorYWm&action=employers&q=software&userip=192.168.43.42&useragent=Chrome/%2F4.0')
+print(response.status_code)
