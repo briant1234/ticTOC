6830beae4028b9d6667fd36eaef8206e7d34bcd7
jennywzhu <jenny.zhu84@gmail.com>
2017-09-09 10:57:02 -0400

    ip

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 print("Hello world")
 
-from pip.vendor import requests
-response = requests.get('http://api.glassdoor.com/api/api.htm?v=1.1&format=json&t.p=193675&t.k=dKoyQlorYWm&action=employers&q=software&userip=192.168.43.42&useragent=Chrome/%2F4.0')
+import requests
+response = requests.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=193675&t.k=dKoyQlorYWm&action=employers&q=software&userip=192.168.43.42&useragent=Chrome/43.0.2357.81', headers={
+                               "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"})
 print(response.status_code)
+
